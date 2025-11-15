82c8a74db15ecd64d988ec38cd7c0bcb704e193c
SabbirAhmad <sabbir.ahmad1@g.bracu.ac.bd>
2025-11-15 22:02:26 +0600

    added updated yml

--- config/reverb.php
+++ config/reverb.php
@@ -4,32 +4,28 @@ return [
     'default' => env('REVERB_SERVER', 'reverb'),
 
     'servers' => [
-        'reverb' => [
-            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
-            'port' => env('REVERB_SERVER_PORT', 6001),
-            'path' => env('REVERB_SERVER_PATH', ''),
-            'hostname' => env('REVERB_HOST'),
-            'options' => [
-                'tls' => [],
-            ],
-            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
-            'scaling' => [
-                'enabled' => env('REVERB_SCALING_ENABLED', false),
-                'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
-                'server' => [
-                    'url' => env('REDIS_URL'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'port' => env('REDIS_PORT', '6379'),
-                    'username' => env('REDIS_USERNAME'),
-                    'password' => env('REDIS_PASSWORD'),
-                    'database' => env('REDIS_DB', '0'),
-                    'timeout' => env('REDIS_TIMEOUT', 60),
-                ],
-            ],
-            'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
-            'telescope_ingest_interval' => env('REVERB_TELESCOPE_INGEST_INTERVAL', 15),
+    'reverb' => [
+        'host' => '0.0.0.0',
+        'port' => 6001, // Hardcoded
+        'path' => '',
+        'hostname' => 'api.shaadimartbd.com',
+        'options' => [
+            'tls' => [],
         ],
+        'max_request_size' => 10000,
+        'scaling' => [
+            'enabled' => false,
+            'channel' => 'reverb',
+            'server' => [
+                'host' => '127.0.0.1',
+                'port' => '6379',
+                'database' => '0',
+            ],
+        ],
+        'pulse_ingest_interval' => 15,
+        'telescope_ingest_interval' => 15,
     ],
+],
 
     'apps' => [
         'provider' => 'config',
